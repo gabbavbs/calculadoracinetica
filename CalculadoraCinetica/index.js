4e72f758d7a75e729673d35bc133a94b5537b0f4
Gabriel <86894966+gabbavbs@users.noreply.github.com>
2024-05-09 09:59:33 -0300

    Add files via upload

---
+++ CalculadoraCinetica/index.js
@@ -0,0 +1,17 @@
+function calcularVelocidade() {
+    var k = parseFloat(document.getElementById('kValue').value);
+    var concentracaoInicial = parseFloat(document.getElementById('initialConcentration').value);
+    var tempo = parseFloat(document.getElementById('time').value);
+    
+    var velocidade = k * concentracaoInicial;
+
+    document.getElementById('resultado').innerHTML = "A velocidade da reação é: " + velocidade.toFixed(2) + " M/s";
+}
+function calcularVelocidade() {
+    var k = parseFloat(document.getElementById('kValue').value);
+    var concentracaoInicial = parseFloat(document.getElementById('initialConcentration').value);
+    
+    var velocidade = (k * concentracaoInicial);
+
+    document.getElementById('resultado').innerHTML = "A velocidade da reação é: " + velocidade.toFixed(3) + " M/s";
+}
